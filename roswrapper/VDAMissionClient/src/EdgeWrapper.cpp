511e00be3ec8f76f2214f3a3b8f5350b011f497f
dzungapr6th99 <dzung.apr6th@gmail.com>
2025-11-22 17:28:19 +0700

    contnue wrapper for edge

--- roswrapper/VDAMissionClient/src/EdgeWrapper.cpp
+++ roswrapper/VDAMissionClient/src/EdgeWrapper.cpp
@@ -1,5 +1,6 @@
 #include "VDA5050Wrapper/EdgeWrapper.hpp"
 
+using Action = vda5050_msgs::msg::Action;
 EdgeWrapper::EdgeWrapper()
 {
 }
@@ -23,5 +24,70 @@ extern "C"
     void Edge_SetDescription(EdgeWrapper *edgeWrapper, const char *data, int length)
     {
         edgeWrapper->entity.edge_description.assign(data, length);
+    }
+
+    void Edge_SetRelease(EdgeWrapper *edgeWrapper, bool data)
+    {
+        edgeWrapper->entity.released = data;
+    }
+
+    void Edge_SetStartNodeId(EdgeWrapper *edgeWrapper, char *data, int length)
+    {
+        edgeWrapper->entity.start_node_id.assign(data, length);
+    }
+
+    void Edge_SetEndNodeId(EdgeWrapper *edgeWrapper, char *data, int length)
+    {
+        edgeWrapper->entity.end_node_id.assign(data, length);
+    }
+
+    void Edge_SetMaxSpeed(EdgeWrapper *edgeWrapper, double data)
+    {
+        edgeWrapper->entity.max_speed = data;
+    }
+
+    void Edge_SetMaxHeight(EdgeWrapper *edgeWrapper, double data)
+    {
+        edgeWrapper->entity.max_height = data;
+    }
+
+    void Edge_SetMinHeight(EdgeWrapper *edgeWrapper, double minHeight)
+    {
+        edgeWrapper->entity.min_height = minHeight;
+    }
+
+    void Edge_SetOrentation(EdgeWrapper *edgeWrapper, double data)
+    {
+        edgeWrapper->entity.orientation = data;
+    }
+
+    void Edge_SetOrientiationType(EdgeWrapper *edgeWrapper, char *data, int length)
+    {
+        edgeWrapper->entity.orientation_type.assign(data, length);
+    }
+
+    void Edge_SetDirection(EdgeWrapper *edgeWrapper, char *data, int length)
+    {
+        edgeWrapper->entity.direction.assign(data, length);
+    }
+
+    void Edge_SetRotationAllowed(EdgeWrapper *edgeWrapper, bool data)
+    {
+        edgeWrapper->entity.rotation_allowed = data;
+    }
+
+    void Edge_SetMaxRotationSpeed(EdgeWrapper *edgeWrapper, double data)
+    {
+        edgeWrapper->entity.max_rotation_speed = data;
+    }
+
+    void Edge_SetLength(EdgeWrapper *edgeWrapper, double data)
+    {
+        edgeWrapper->entity.length = data;
+    }
+
+     void Edge_SetAction(EdgeWrapper* edgeWrapper, const Action* actions, int length)
+    {
+        edgeWrapper->entity.actions = std::vector<Action>(actions, actions + length);
     }
 }
