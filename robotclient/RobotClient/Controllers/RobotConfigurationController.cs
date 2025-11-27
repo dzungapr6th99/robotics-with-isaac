bfb4543bde86de55adbfc3ace7bf5cfe034f9321
dzungapr6th99 <dzung.apr6th@gmail.com>
2025-11-27 19:10:50 +0700

    Add controller for config robot

---
+++ robotclient/RobotClient/Controllers/RobotConfigurationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RobotClient.Controllers
+{
+    [ApiController]
+    [Route("robot-configuration")]
+    public class RobotConfigurationController : ControllerBase
+    {
+        [HttpPost("vda5050")]
+        public Task<IActionResult> ConfigVDA()
+        {
+            return Ok(new object()
+            {
+                
+            });
+        }
+
+        public Task<IActionResult> ConfigMqtt()
+        {
+            return Ok(new object());
+        }
+
+    }
+}
