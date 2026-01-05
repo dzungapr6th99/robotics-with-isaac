99be765296681e6aa57e5e72e9446f105aa1809b
dzungapr6th99 <dzung.apr6th@gmail.com>
2026-01-05 18:54:24 +0700

    update new code

--- robotclient/RosNodeWrapper/VDARosClient.cs
+++ robotclient/RosNodeWrapper/VDARosClient.cs
@@ -1,4 +1,5 @@
 ﻿using ConfigApp;
+using Disruptor;
 using System.Runtime.InteropServices;
 using System.Text;
 using VDA5050Message;
@@ -10,45 +11,57 @@ namespace RosNodeWrapper
         private IntPtr _nodeVDA;
 
         private bool _isRunningNode = false;
-        
+
         #region Wrapper function
         internal const string _libVDAClient = "libVDAMissionClient.so";
         internal const string _libVDAMsg = "libVDAMessage.so";
         [DllImport(_libVDAClient)]
         public static extern void InitEnviroment();
         [DllImport(_libVDAClient, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-        private static extern IntPtr CreateVDANode(string robotNamespace);
+        private static extern IntPtr CreateVDAMissionClient(string robotNamespace);
 
         [DllImport(_libVDAClient)]
-        private static extern void SpinNode();
+        private static extern void SpinNode(IntPtr nodePtr);
 
         [DllImport(_libVDAClient)]
-        private static extern IntPtr GetCurrentVisualization();
+        private static extern IntPtr GetVisualization();
 
         [DllImport(_libVDAClient)]
-        private static extern IntPtr GetCurrentRobotState();
+        private static extern IntPtr GetAGVState();
 
         [DllImport(_libVDAClient)]
         private static extern bool RunThroughPoses(IntPtr nodePtr, double[] xs, double[] ys, int n, double theta_final_rad, byte[] order_id);
-        
+
         [DllImport(_libVDAClient)]
         private static extern bool ExecuteOrder(IntPtr nodePtr, IntPtr orderPtr);
 
         [DllImport(_libVDAClient)]
         private static extern bool ExecuteInstantActions(IntPtr nodePtr, IntPtr instantActionsPtr);
 
+        private List<IEventHandler<VDA5050MessageBase>> _listMessageQueueService;
+
+        private Thread? _threadSpinNode;
         #endregion
         public VDARosClient()
         {
-
-            _nodeVDA = CreateVDANode(ConfigData.RosNamespace);
+            _listMessageQueueService = new List<IEventHandler<VDA5050MessageBase>>();
+            _nodeVDA = CreateVDAMissionClient(ConfigData.RosNamespace);
         }
 
+        public void StartSpinNode()
+        {
+            if (_threadSpinNode == null || !_threadSpinNode.IsAlive)
+            {
+                _threadSpinNode = new Thread(threadSpinNodeVDA);
+                _threadSpinNode.IsBackground = true;
+                _threadSpinNode.Start();
+            }
+        }
 
         public void ExecuteOrder(Order order)
         {
 
-            IntPtr? orderWrapperPtr =  order.GetWrapperPtr();
+            IntPtr? orderWrapperPtr = order.GetWrapperPtr();
             if (orderWrapperPtr != null && orderWrapperPtr.HasValue)
             {
                 ExecuteOrder(_nodeVDA, (IntPtr)orderWrapperPtr);
@@ -65,9 +78,29 @@ namespace RosNodeWrapper
             }
         }
 
-        private void threadPublishRobotData()
+        public void SubscribeData(IEventHandler<VDA5050MessageBase> queueMsgService)
         {
-            
+            _listMessageQueueService.Add(queueMsgService);
+        }
+
+        public void UnsubscribeData(IEventHandler<VDA5050MessageBase> queueMsgService)
+        {
+            _listMessageQueueService.Remove(queueMsgService);
+        }
+
+        private void threadSpinNodeVDA()
+        {
+            while (_isRunningNode)
+            {
+                SpinNode(_nodeVDA);
+                IntPtr ptrVisualization = GetVisualization();
+                IntPtr ptrState = GetAGVState();
+                Visualization visualization = new Visualization();
+                visualization.GetDataWrapper(ptrVisualization);
+                State state = new State();
+                state.GetDataWrapper(ptrState);
+
+            }
         }
     }
 }
